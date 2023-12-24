d5b8cc0c6113c47afc0144eeae44207aae4f0ca3
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-12-24 11:08:46 +0200

    add popup name

--- xo-game/src/Components/Start/Start.jsx
+++ xo-game/src/Components/Start/Start.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
 import Xicon from "../icons/Xicon";
 import Oicon from "../icons/Oicon";
+import PoupupName from '../PoupupName/PoupupName';
 import "./start.css";
 
 export default function Start() {
   const [activePlayer, setActivePlayer] = useState("X");
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [PlayerX, setPlayerX] = useState("");
+  const [PlayerO, setPlayerO] = useState("");
 
   const handlePlayerClick = (player) => {
     setActivePlayer(player);
+  };
+
+  const handleTogglePopup = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+
+  const handleAddName = (name) => {
+    if (activePlayer === "X") {
+      setPlayerX(name);
+    } else if (activePlayer === "O") {
+      setPlayerO(name);
+    }
+    handleTogglePopup();
   };
 
   return (
@@ -37,14 +54,24 @@ export default function Start() {
             <p className="text-light text-normal">remember: x goes first</p>
           </div>
           <div className="start_btns">
-            <button className="btn btn-yellow">
+            <button className="btn btn-yellow" onClick={handleTogglePopup}>
               Enter the name for player
               <span style={{ margin: "0 5px", fontSize: "20px" }}>
                 {" "}
                 {activePlayer}{" "}
               </span>
             </button>
+            {PlayerX !== "" && PlayerO !== "" && (
+              <button className="btn btn-blue">Start Game</button>
+            )}
           </div>
+          {isPopupOpen && (
+            <PoupupName
+              togglePopup={handleTogglePopup}
+              activePlayer={activePlayer}
+              addName={handleAddName}
+            />
+          )}
         </div>
       </div>
     </div>
