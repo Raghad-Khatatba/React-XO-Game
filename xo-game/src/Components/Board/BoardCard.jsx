e50fef1eccdbfec1fe16b1618aa2c134fd13a662
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-12-26 18:07:47 +0200

    handel square click and handel X or O turn and handel Restart Game

--- xo-game/src/Components/Board/BoardCard.jsx
+++ xo-game/src/Components/Board/BoardCard.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Xicon from "../icons/Xicon";
 import Oicon from "../icons/Oicon";
 import "./board.css";
-export default function BoardCard({ user, active, index }) {
+export default function BoardCard({ user, active, index , onClick  }) {
   return (
     <div
       className={`card ${active && user === "X" && "shadow-blue"} ${
         active && user === "O" && "shadow-yellow"
       } ${!active ? "shadow-gray" : "active"}`}
+      onClick={onClick}
     >
       {user === "X" && <Xicon color={active ? "dark" : undefined} size="lg" />}
       {user === "O" && <Oicon color={active ? "dark" : undefined} size="lg" />}
