ace0dd1e1514aee9dc788684cc8b31c06f883a9a
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-12-26 16:02:07 +0200

    add AppRouting

--- xo-game/src/App.js
+++ xo-game/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Start from './Components/Start/Start';
-import Board from './Components/Board/Board';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from './Components/AppRoutes/AppRoutes';
+
+
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Start />} />
-          <Route path="/board" element={<Board />} />
-   </Routes>
-   </BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
